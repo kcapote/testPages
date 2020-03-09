e83741747b175b7df620ea00a2ba39b47ff09479
Kenny Capote <kennycapote@MacBook-Air-de-Kenny.local>
2020-03-08 22:33:11 -0300

    first commit

---
+++ docs/precache-manifest.be82b10554d9b747f8cac150eead7a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd29ba1046a2a88e693715f43109c657",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f7aea7973a37de73f47",
+    "url": "/static/css/main.244189ea.chunk.css"
+  },
+  {
+    "revision": "17b03a12f8d1717d5f39",
+    "url": "/static/js/2.26bf3cc3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.26bf3cc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f7aea7973a37de73f47",
+    "url": "/static/js/main.d2f19728.chunk.js"
+  },
+  {
+    "revision": "24e0e6e6d999e11d51d9",
+    "url": "/static/js/runtime-main.66e88cb1.js"
+  }
+]);
